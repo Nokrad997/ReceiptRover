b887d23843e4631907d1977cfeeb047f89da6cc9
EskamXD <58010243+EskamXD@users.noreply.github.com>
2024-01-17 19:43:18 +0100

    added view

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from src.models.Users import Users
-import bcrypt
+import tkinter as tk
+import ttkbootstrap as ttk
 
-from src.controllers.RegistrationController import RegistrationController
-from src.controllers.LoginController import LoginController
+# from src.models.Users import Users
+# import bcrypt
 
-from src.views.RegistrationView import RegistrationView
+# from src.controllers.RegistrationController import RegistrationController
+# from src.controllers.LoginController import LoginController
+
+from src.Navigator import Navigator
 from src.views.LoginView import LoginView
+from src.views.MainView import MainView
+from src.views.RegistrationView import RegistrationView
 
 # usr = Users()
 # rv = RegistrationView("test", "test@log1.pl", "testtest", "testtest")
@@ -13,11 +18,28 @@ from src.views.LoginView import LoginView
 
 # print(rc.registerUser())
 
-usr = Users()
-lv = LoginView("test@log1.pl", "testtest")
-lc = LoginController(lv, usr)
+# usr = Users()
+# lv = LoginView("test@log1.pl", "testtest")
+# lc = LoginController(lv, usr)
 
-print(lc.login())
+# print(lc.login())
 
 # print(bcrypt.checkpw('testtest'.encode('utf-8'), "$2b$12$b6derw0r4Sw2wYE0C2GuU.ecVTku5uzGQtvw3YO/ZyIvYNTJTyLQW".encode('utf-8')))
 # print(bcrypt.checkpw('testtest'.encode('utf-8'), "$2b$12$xqLrHQyUN5OOeDlCilkvFOe3GVr0/mG6tckQ2IQjiWgCTz8QhV8Bi".encode('utf-8')))
+
+windows = ttk.Window(themename="superhero")
+windows.title("Test")
+windows.geometry("321x694")
+windows.resizable(False, False)
+
+mainViewCanvas = tk.Canvas(windows)
+mainView = MainView(mainViewCanvas)
+Navigator().navigateTo(mainView)
+
+# loginViewCanvas = tk.Canvas(windows)
+# loginView = LoginView(loginViewCanvas, "email", "password")
+
+# registerViewCanvas = tk.Canvas(windows)
+# registerView = RegistrationView(registerViewCanvas, "chuj", "chuj", "chuj", "chuj")
+
+windows.mainloop()
--- src/views/LoginView.py
+++ src/views/LoginView.py
@@ -1,4 +1,55 @@
-class LoginView:
-    def __init__(self, email, password):
+import tkinter as tk
+from ttkbootstrap import ttk
+
+from src.Navigator import Navigator
+from src.views.View import View
+class LoginView(View):
+    def __init__(self, canvas, email, password):
+        super().__init__(canvas)
         self.email = email
         self.password = password
+
+        self.loginLabel = ttk.Label(self.canvas, text="Login")
+        self.loginEntry = ttk.Entry(self.canvas)
+        self.passwordLabel = ttk.Label(self.canvas, text="Email")
+        self.passwordEntry = ttk.Entry(self.canvas)
+
+        self.loginButton = ttk.Button(self.canvas, text="Login")
+        self.registerButton = ttk.Button(self.canvas, text="Register")
+        self.registerButton.configure(bootstyle="outline")
+
+
+        self.navbarFrame = ttk.Frame(self.canvas)
+        self.navbarFrame.configure(bootstyle="sucess")
+
+        self.backButton = ttk.Button(self.navbarFrame, text="Back")
+        self.backButton.configure(bootstyle="outline", command=lambda: Navigator().navigateBack())
+
+
+    def place(self):
+        self.canvas.place(x=0, y=0, width=321, height=694)
+        
+        self.loginLabel.place(x=60, y=145, width=200, height=20)
+        self.loginEntry.place(x=60, y=165, width=200, height=30)
+        self.passwordLabel.place(x=60, y=205, width=200, height=20)
+        self.passwordEntry.place(x=60, y=225, width=200, height=30)
+
+        self.loginButton.place(x=60, y=285, width=200, height=30)
+        self.registerButton.place(x=60, y=325, width=200, height=30)
+
+        self.navbarFrame.place(x=0, y=624, width=321, height=50)
+        self.backButton.place(x=7, y=10, width=321-14, height=40)
+
+    def hide(self):
+        # self.canvas.place_forget()
+        
+        self.loginLabel.place_forget()
+        self.loginEntry.place_forget()
+        self.passwordLabel.place_forget()
+        self.passwordEntry.place_forget()
+
+        self.loginButton.place_forget()
+        self.registerButton.place_forget()
+
+        self.navbarFrame.place_forget()
+        self.backButton.place_forget()
