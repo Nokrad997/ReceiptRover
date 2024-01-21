a310f78ea97510540b97c2c1722dae396774d1ec
EskamXD <58010243+EskamXD@users.noreply.github.com>
2024-01-21 20:59:49 +0100

    main update

--- main.py
+++ main.py
@@ -3,15 +3,6 @@ import ttkbootstrap as ttk
 from src.Navigator import Navigator
 from src.views.MainView import MainView
 
-<<<<<<< HEAD
-windows = ttk.Window(themename="superhero")
-windows.title("Test")
-windows.geometry("320x700")
-windows.resizable(False, False)
-
-mainViewCanvas = tk.Canvas(windows)
-mainView = MainView(mainViewCanvas)
-=======
 root = ttk.Window(themename="superhero")
 root.title("Test")
 root.geometry("320x700")
@@ -19,7 +10,6 @@ root.resizable(False, False)
 
 mainViewCanvas = tk.Canvas(root)
 mainView = MainView(mainViewCanvas, root)
->>>>>>> add-receipt-controller
 Navigator().navigateTo(mainView)
 
 root.mainloop()
