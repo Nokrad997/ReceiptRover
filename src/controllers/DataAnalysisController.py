4db733ee4e5a3bb866ad344fa900927fd5e8853b
Kondro571 <kondziot2000@gmail.com>
2024-01-24 01:15:10 +0100

    zmiany

--- src/controllers/DataAnalysisController.py
+++ src/controllers/DataAnalysisController.py
@@ -3,25 +3,29 @@ class DataAnalysisController():
     def __init__(self):
         self.service = DataAnalysisService()
     
-    def getMounthlyStatement(self,receipt):
+    def getMounthlyStatement(self,receipt,year,month):
         """
         Generates a chart base on Monthly expenses
         Args:
             receipt: The receipt to be analysed.
+            year: The year
+            month: The month
         Returns:
             str: path to the chart
         """
-        return self.service.getMounthlyStatement(receipt)
+        return self.service.getMounthlyStatement(receipt,year,month)
         
-    def getShopStatement(self,receipt):
+    def getShopStatement(self,receipt,year,month):
         """
         Generates a chart base on expensys from particular shops
         Args:
             receipt: The receipt to be analysed.
+            year: The year
+            month: The month
         Returns:
             str: path to the chart
         """
-        return self.service.getShopStatement(receipt)
+        return self.service.getShopStatement(receipt,year,month)
     
     def clearCharts(self):
         """
