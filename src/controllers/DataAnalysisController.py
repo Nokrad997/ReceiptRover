b47bd47025e78e134f2c0d2ebe7ade06e41e6880
EskamXD <58010243+EskamXD@users.noreply.github.com>
2024-01-24 15:31:38 +0100

    pulled main

--- src/controllers/DataAnalysisController.py
+++ src/controllers/DataAnalysisController.py
@@ -1,35 +1,38 @@
 from src.services.DataAnalysisService import DataAnalysisService
-class DataAnalysisController():
+
+
+class DataAnalysisController:
     def __init__(self):
         self.service = DataAnalysisService()
+
     # jeszcze nie zaimplementowane
-    def getMounthlyStatement(self,receipt,year,month):
+    def getMounthlyStatement(self, receipts, year, month):
         """
         Generates a chart base on Monthly expenses
         Args:
-            receipt: The receipt to be analysed.
+            receipts: The receipt to be analysed.
             year: The year
             month: The month
         Returns:
             str: path to the chart
         """
-        return self.service.getMounthlyStatement(receipt,year,month)
-        
-    def getShopStatement(self,receipt,year,month):
+        return self.service.getMounthlyStatement(receipts, year, month)
+
+    def getShopStatement(self, receipts, year, month):
         """
         Generates a chart base on expensys from particular shops in selected month
         Args:
-            receipt: The receipt to be analysed.
+            receipts: The receipt to be analysed.
             year: The year
             month: The month
         Returns:
             str: path to the chart
         """
-        return self.service.getShopStatement(receipt,year,month)
-    
+        return self.service.getShopStatement(receipts, year, month)
+
     def clearCharts(self):
         """
         Clear folder where charts are stored
-        
+
         """
         self.service.clearCharts()
